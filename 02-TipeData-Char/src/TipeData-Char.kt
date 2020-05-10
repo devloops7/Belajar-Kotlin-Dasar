9f6c1070911b32cade0c108e2dcd650d4cd26edf
dhiyo7 <dhiyoreksa12@gmail.com>
2020-05-10 19:10:43 +0700

    02-TipeData-Car

---
+++ 02-TipeData-Char/src/TipeData-Char.kt
@@ -0,0 +1,31 @@
+fun main() {
+//    var char1: Char = 'D'
+//    var char2: Char = 'E'
+//    var char3: Char = 'V'
+//    var char4: Char = 'L'
+//    var char5: Char = 'O'
+//    var char6: Char = 'O'
+//    var char7: Char = 'P'
+//    var char8: Char = 'S'
+//    var char9: Char = '7'
+
+    var char1 = 'D'
+    var char2 = 'E'
+    var char3 = 'V'
+    var char4 = 'L'
+    var char5 = 'O'
+    var char6 = 'O'
+    var char7 = 'P'
+    var char8 = 'S'
+    var char9 = '7'
+
+    print(char1)
+    print(char2)
+    print(char3)
+    print(char4)
+    print(char5)
+    print(char6)
+    print(char7)
+    print(char8)
+    print(char9)
+}
